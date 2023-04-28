8a3fc18d34663ffb4a61c8ad6172dd8d4c7c1593
JS junior <arnabgogoi83@gmail.com>
2023-04-28 07:42:35 +0530

    added review functionality

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,10 @@
 import { useState, useEffect, useRef } from "react";
-import Navbar from "../../components/navbar"
+import Navbar from "../components/navbar"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import{ onAuthStateChanged } from "firebase/auth";
-import { db, auth } from "../../components/firebase"
-import CheckAuthPopup from "../../components/checkAuthPopup"
+import { db, auth } from "../components/firebase"
+import CheckAuthPopup from "../components/checkAuthPopup"
 import { collection, getDoc, getDocs, doc, addDoc, documentId, where, query, updateDoc, setDoc, deleteDoc, serverTimestamp } from "firebase/firestore"
 import { AiOutlineSend } from "react-icons/fa"
 import SearchIcon from "@heroicons/react"
